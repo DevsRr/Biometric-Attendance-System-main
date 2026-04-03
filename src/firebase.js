8b85fa08349b208ab3aacbeb5c9cff33668e94f6
DevsRr <deveraralph@dmcfi.edu.ph>
2026-04-03 23:47:22 +0800

    New Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCupcKt2uioLQcyj0LrNGwIwXZ7MYDVsKE",
-  authDomain: "biometric-f0000.firebaseapp.com",
-  projectId: "biometric-f0000",
-  storageBucket: "biometric-f0000.firebasestorage.app",
-  messagingSenderId: "164846431061",
-  appId: "1:164846431061:web:03b1eebb018e2038c09ddc",
-  measurementId: "G-KEQZ95KQHE"
+  apiKey: "AIzaSyBj_fhhi2Rd9zhX4GR9iV9Lud0tnD1Dn58",
+  authDomain: "flood-monitoring-6a4d0.firebaseapp.com",
+  databaseURL: "https://flood-monitoring-6a4d0-default-rtdb.firebaseio.com",
+  projectId: "flood-monitoring-6a4d0",
+  storageBucket: "flood-monitoring-6a4d0.firebasestorage.app",
+  messagingSenderId: "514740774560",
+  appId: "1:514740774560:web:1bbcc150ca38e0b07e0bc5",
+  measurementId: "G-92DTLSV8WY"
 };
 
 const app = initializeApp(firebaseConfig);
